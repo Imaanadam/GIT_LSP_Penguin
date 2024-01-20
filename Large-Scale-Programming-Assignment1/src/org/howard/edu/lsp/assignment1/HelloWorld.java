093f9e937c2ad7f5a2b070288bbc227d6fb0bbf7
imaan <imaan@LAPTOP-49QUKARJ>
2024-01-19 19:38:49 -0500

    this is the source code for assignment 1

---
+++ Large-Scale-Programming-Assignment1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,18 @@
+
+
+/**
+*  Name: Imaan Adam
+*/
+
+
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
